ccd1ad506240414f98ca496b3aa754186da84df7
Karan Doshi <73486482+doshikaran@users.noreply.github.com>
2023-01-30 14:49:36 -0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app";
 import {getAuth, } from 'firebase/auth'
 const firebaseConfig = {
   apiKey: "",
-  authDomain: "quote-42bc0.firebaseapp.com",
-  projectId: "quote-42bc0",
-  storageBucket: "quote-42bc0.appspot.com",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
   messagingSenderId: "",
   appId: "",
   measurementId: ""
